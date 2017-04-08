592c36b13f38376adee9c266c17621cff6809423
wiseman <wiseman@rukoo>
2017-04-08 20:23:12 +0700

    init commit

---
+++ src/main/java/ru/votrin/doctordata/DoctorUiApplication.java
@@ -0,0 +1,12 @@
+package ru.votrin.doctordata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorUiApplication.class, args);
+	}
+}
