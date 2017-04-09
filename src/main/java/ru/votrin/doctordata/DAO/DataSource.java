3c94873b2a636697dcb840e8bbd31d6da4187d33
wiseman <votrin.and@yandex.ru>
2017-04-09 14:43:20 +0700

    Temporary removed property placeholder

--- src/main/java/ru/votrin/doctordata/DAO/DataSource.java
+++ src/main/java/ru/votrin/doctordata/DAO/DataSource.java
@@ -1,7 +1,13 @@
 package ru.votrin.doctordata.DAO;
 
+import com.sun.deploy.Environment;
 import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.stereotype.Component;
 
 /**
@@ -10,19 +16,21 @@ import org.springframework.stereotype.Component;
 
 
 @Component
+@Configuration
+@PropertySource("classpath:application.properties")
 public class DataSource extends BasicDataSource {
-    @Value("${spring.datasource.driver-class-name}")
-    private String className;
 
-    @Value("${spring.datasource.url}")
-    private String url;
+//    @Value("${spring.datasource.driver-class-name}")
+    private String className = "org.postgresql.Driver";
 
-    @Value("${spring.datasource.username}")
-    private String username;
+//    @Value("${spring.datasource.url}")
+    private String url = "jdbc:postgresql://trollcave:5432/info";
 
-    @Value("${spring.datasource.password}")
-    private String password;
+//    @Value("${spring.datasource.username}")
+    private String username = "admin";
 
+//    @Value("${spring.datasource.password}")
+    private String password = "admin";
 
     public DataSource() {
         super();
@@ -31,4 +39,10 @@ public class DataSource extends BasicDataSource {
         setUsername(username);
         setPassword(password);
     }
+/*
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfig() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+ */
 }
